5fd978f36ecebc91e170fd191680aa974850b0ba
wanghaisheng <edwin_uestc@163.com>
2024-06-29 04:36:22 +0800

    change to playwright

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,23 +1,25 @@
+export const maxDuration = 5; // This function can run for a maximum of 5 seconds
+export const dynamic = 'force-dynamic';
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
 const CHROMIUM_PATH =
-  "https://vomrghiulbmrfvmhlflk.supabase.co/storage/v1/object/public/chromium-pack/chromium-v123.0.0-pack.tar";
-
+  // "https://vomrghiulbmrfvmhlflk.supabase.co/storage/v1/object/public/chromium-pack/chromium-v123.0.0-pack.tar";
+ "https://github.com/Sparticuz/chromium/releases/download/v123.0.0/chromium-v123.0.0-pack.tar"
 async function getBrowser() {
   if (process.env.VERCEL_ENV === "production") {
     const chromium = await import("@sparticuz/chromium-min").then(
       (mod) => mod.default
     );
-
-    const puppeteerCore = await import("puppeteer-core").then(
+    
+    const  { chromium: playwright }  = await import("playwright-core").then(
       (mod) => mod.default
     );
 
     const executablePath = await chromium.executablePath(CHROMIUM_PATH);
 
-    const browser = await puppeteerCore.launch({
+    const browser = await playwright.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
       executablePath,
@@ -25,23 +27,45 @@ async function getBrowser() {
     });
     return browser;
   } else {
-    const puppeteer = await import("puppeteer").then((mod) => mod.default);
+    const  { chromium: playwright }  = await import("playwright-core").then(
+      (mod) => mod.default);
 
-    const browser = await puppeteer.launch();
+    const browser = await playwright.launch();
     return browser;
   }
 }
 
 export async function GET(request: NextRequest) {
-  const browser = await getBrowser();
 
-  const page = await browser.newPage();
-  await page.goto("https://example.com");
-  const pdf = await page.pdf();
-  await browser.close();
-  return new NextResponse(pdf, {
-    headers: {
-      "Content-Type": "application/pdf",
-    },
-  });
+  const browser = await getBrowser();
+  const url = request.nextUrl.searchParams.get("url") || null;
+  if(url){
+
+    const page = await browser.newPage();
+    await page.goto("https://radar.cloudflare.com/scan");
+    await page.locator(".a op oq or os").fill(url)
+    await page.locator('#root > main > div.radar-grid.gf.bk.bh.ak.gg.gh > form > fieldset > div > button')
+    await page.click('#root > main > div.radar-grid.gf.bk.bh.ak.gg.gh > form > fieldset > div > button')
+    await page.locator('.radar-link bf ea dj ds dr').isVisible()
+    const uuid = await page.locator('a.radar-link bf ea dj ds dr').getAttribute('href')
+    // const spanText = await page.$eval('span.mySpan', span => span.textContent);
+  
+    await browser.close();
+    return new NextResponse({'url':url,'uuid':uuid}, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+  }else{
+    return new NextResponse({'url':url,'uuid':'check your input and try again'}, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+
+
+  }
+
 }
